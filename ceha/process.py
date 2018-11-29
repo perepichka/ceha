ceade270f0c77514aff725e5e84222001e3c1e71
Van Duong <vanduong1610@gmail.com>
2018-11-29 17:45:34 -0500

    add SL0 file and complete compression/encryption

--- ceha/process.py
+++ ceha/process.py
@@ -6,13 +6,19 @@ by the paper
 import cv2
 import sys
 import logging
-import compress.py as cp
-import random_exchange.py as re
+import compress as cp
+import random_exchange as re
 import numpy as np
-from pyCSalgos import SmoothedL0 as sl
+import SL0 as sl
+import os
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-
+log_map1 = None
+log_map2 = None
+phi1 = None
+phi2 = None
+r1 = None
+r2 = None
 def read_into_blocks(img):
     img_height, img_width = img.shape[:2]
     #divide the array into 4 blocks, block 1 is at top left corner, going counter-clockwise
@@ -29,27 +35,37 @@ def compress_encrypt():
     compress and encrypt input image
     """
 
+    global log_map1, log_map2, phi1, phi2, r1, r2
+
     #read image from command line
-    img = cv2.imread(sys.argv[1])
+    img = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
     img_width, img_height = img.shape[:2]
+    if len(img.shape) == 3:
+        print("image still 3-D")
+        #img = img[:,:,0]
+
 
     #Step 1: read and divide into blocks
-    blocks = read_into_blocks(img)
+    b1, b2, b3, b4 = read_into_blocks(img)
+    print("size of b1: ", b1.shape)
 
     #Step 2: measure blocks with phi_matrix
     lamb = 2
-    log_map1 = cp.logistic_map(0.11, img_width)
-    log_map2 = cp.logistic_map(0.23, img_width)
+    log_map1 = cp.logistic_map(0.11, img_width//2)
+    log_map2 = cp.logistic_map(0.23, img_width//2)
     phi1 = cp.phi_matrix(log_map1, lamb)
     phi2 = cp.phi_matrix(log_map2, lamb)
-    c1 = phi1.dot(blocks[0])
-    c3 = phi1.dot(blocks[2])
-    c2 = phi2.dot(blocks[1])
-    c4 =  phi2.dot(blocks[3])
+    print("size of log_map1: ", log_map1.shape[:2])
+    print("size of phi1: ", phi1.shape[:2])
+    print("size of b1: ", b1.shape[:2])
+    c1 = phi1.dot(b1)
+    c3 = phi1.dot(b3)
+    c2 = phi2.dot(b2)
+    c4 =  phi2.dot(b4)
 
     #Step 3: pixel exchange
-    r1 = R_matrix(phi1, lamb)
-    r2 = R_matrix(phi2, lamb)
+    r1 = re.R_matrix(phi1, lamb)
+    r2 = re.R_matrix(phi2, lamb)
     #e1 is block1 after exchange
     e1, e2 = re.rand_pixel_exchange(c1, c2, r1)
     e2, e3 = re.rand_pixel_exchange(c2, c3, r2)
@@ -61,7 +77,8 @@ def compress_encrypt():
 
 def decompress_decrypt():
     #Step 1: read into blocks 
-    img = cv2.imread(sys.argv[1])
+    #img = cv2.imread(sys.argv[1])
+    img = compress_encrypt()
     c1, c2, c3, c4 = read_into_blocks(img)
 
     #Step 2: inverse pixel exchange
@@ -69,15 +86,25 @@ def decompress_decrypt():
     d3, d4 = re.rand_pixel_exchange(c3, c4, r1)
     d2, d3 = re.rand_pixel_exchange(c2, c3, r2)
     d1, d2 = re.rand_pixel_exchange(c1, c2, r1)
-
+    print("size of d4: ", d4.shape)
+    print("phi1 size: ", phi1.shape)
     #Step 3: run SL0 algorithm with phi matrix
+    sigma_min = 1
+    alpha1 = sl.SL0(cv2.dct(phi1), d1.flatten(), sigma_min)
+    alpha2 = sl.SL0(cv2.dct(phi2), d2.flatten(), sigma_min)
+    alpha3 = sl.SL0(cv2.dct(phi1), d3.flatten(), sigma_min)
+    alpha4 = sl.SL0(cv2.dct(phi2), d4.flatten(), sigma_min)
+    print("size of alpha1: ", alpha1.shape)
 
+    #convert back to space domain
+    b1 = cv2.idct(alpha1)
+    b2 = cv2.idct(alpha2)
+    b3 = cv2.idct(alpha3)
+    b4 = cv2.idct(alpha4)
 
     #Step 4: constrct the decripted image
+    return np.bmat([[b1, b2] [b3, b4]])
 
 if __name__ == '__main__':
-    result = read_into_blocks()
-    print("first block size: ", result[0].shape[:2][0], " ", result[0].shape[:2][1])
-    print("second block size: ", result[1].shape[:2][0], " ", result[1].shape[:2][1])
-    print("third block size: ", result[2].shape[:2][0], " ", result[2].shape[:2][1])
-    print("fourth block size: ", result[3].shape[:2][0], " ", result[3].shape[:2][1])
+    result = decompress_decrypt()
+    print("size of result: ", result.shape)
--- ceha/random_exchange.py
+++ ceha/random_exchange.py
@@ -15,14 +15,14 @@ b1, b2 are two blocks of matrix
 R is a random matrix
 Output: 2 blocks of matrix after pixel exchange
 """
-def rand_pixel_exchange(b1, b2, R):
-	R_width, R_height = R.shape[:2]
+def rand_pixel_exchange(b1, b2, r):
+	R_height, R_width = r.shape[:2]
 
 	#calculate sum of all values in R
 	sum = 0
 	for m in range(0, R_height - 1):
 		for n in range(0, R_width - 1):
-			sum = sum + R[m][n]
+			sum = sum + r[m][n]
 
 	R_bar = 1/(R_width*R_height)*sum
 
@@ -30,9 +30,9 @@ def rand_pixel_exchange(b1, b2, R):
 		for n in range(0, len(b1[0]) - 1):
 			#since the range of m and n in the paper starts from 1,
 			#the following formula is a bit different from the one in the paper
-			 new_m = 1 + int(round((R_height - 2) * math.sin(math.pi * R[m][n])))
-			 new_n = 1 + int(round((R_width - 2) * R[m][n]))
-			 if R[m][n] > R_bar:
+			 new_m = 1 + int(round((R_height - 2) * math.sin(math.pi * r[m][n])))
+			 new_n = 1 + int(round((R_width - 2) * r[m][n]))
+			 if r[m][n] > R_bar:
 			 	temp = b1[new_m][new_n]
 			 	b1[new_m][new_n] = b2[m][n]
 			 	b2[m][n] = temp
@@ -51,18 +51,18 @@ def rand_pixel_exchange(b1, b2, R):
 
 if __name__ == '__main__':
 	phi_matrix = np.random.rand(4,4)
-	R = R_matrix(phi_matrix, 2)
+	r = R_matrix(phi_matrix, 2)
 	print("Random Matrix R")
-	print(R)
-	A = np.random.rand(4, 4)
-	B = np.random.rand(4, 4)
+	print(r)
+	a = np.random.rand(4, 4)
+	b = np.random.rand(4, 4)
 	print("Matrix A: ")
-	print(A)
+	print(a)
 	print("Matrix B: ")
-	print(B)
-	r = rand_pixel_exchange(A, B, R)
+	print(b)
+	d = rand_pixel_exchange(a,b,r)
 	print("Result matrix A: ")
-	print( r[0])
+	print( d[0])
 	print("Result matrix B: " )
-	print(r[1])
+	print(d[1])
 
